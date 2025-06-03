5b30f5535d303eaec7b09ce41cdf322ea693a46f
vsrnp <vsrnpra2@gmail.com>
2025-06-03 19:07:30 +0300

    t6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import Tas4.BubbleSort;
 
 public class Main {
-    int[] m = new int[]{1,76,12,23,56,1,6,56};
+    public static void main(String[] args) {
+
+    }
 
 }
---
+++ src/Task6/CustomSet.java
@@ -0,0 +1,54 @@
+package Task6;
+
+
+import java.util.HashMap;
+
+
+public class CustomSet<T> {
+    private static final Object PRESENT = new Object();
+    private final HashMap<T, Object> map;
+
+    public CustomSet() {
+        this.map = new HashMap<>();
+    }
+
+    public boolean add(T element) {
+        return map.put(element, PRESENT) == null;
+    }
+
+    public void remove(T element) {
+        map.remove(element);
+    }
+
+    public boolean contains(T element) {
+        return map.containsKey(element);
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public void clear() {
+        map.clear();
+    }
+    public String toString() {
+        return map.keySet().toString().replace("[", "").replace("]", "");
+    }
+
+    public static void main(String[] args) {
+        CustomSet<String> customSet = new CustomSet<>();
+        customSet.add("aboba");
+        customSet.add("bob");
+        customSet.add("bob2");
+        customSet.add("sigiroyven");
+        System.out.println(customSet);
+        customSet.remove("aboba");
+        System.out.println(customSet);
+        System.out.println(customSet.isEmpty());
+        System.out.println(customSet.contains("bob"));
+    }
+}
